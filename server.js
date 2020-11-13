47ce04814cc9d003adfcb80a2e5c8e66a0b71c72
Idriss Amraoui <idrissamraoui@protonmail.com>
2020-11-13 08:44:59 -0500

    Added routes

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require ('express'); 
-const app = express ();
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to the Contact Keeper API'}))
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the Contact Keeper API' })
+);
+
+// Define routes
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// Setting up server & PORT
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log (`Server Started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
