fc8246f0cca68855850ca5c7b8b7a63929c5e768
Economicsnerd <idrissamraoui@protonmail.com>
2020-11-20 09:35:15 -0500

    Add user route & validation. DB is saving user correctly

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,54 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  "/",
+  [
+    check("name", "please add name").not().isEmpty(),
+    check("email", "please include valid email").isEmail(),
+    check(
+      "password",
+      "please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
+    try {
+      // Check first if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+      // Create user instance using User Model
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      // Encrypt & Store new user in DB
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      res.send("User Saved");
+    } catch {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
 // Connect DB
 connectDB();
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the Contact Keeper API' })
+// init middleware
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the Contact Keeper API" })
 );
 
 // Define routes
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 // Setting up server & PORT
 
