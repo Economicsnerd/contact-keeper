689e00ed64bef5458387b6f448e1903438d84419
Economicsnerd <idrissamraoui@protonmail.com>
2020-11-23 08:44:35 -0500

    Added contact model

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,32 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 // @route   GET api/contacts
 // @desc    Get all user contacts
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.send("Server Error");
+  }
 });
 
 // @route   POST api/contacts
 // @desc    Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add contact');
+router.post("/", (req, res) => {
+  res.send("Add contact");
 });
 
 // @route   PUT api/contacts/:id
 // @desc    Update contact
 // @access  Private
-router.put('/:id', (req, res) => {
-  res.send('Update contact');
+router.put("/:id", (req, res) => {
+  res.send("Update contact");
 });
 
 // @route   DELETE api/contacts/:id
 // @desc    Delete contact
 // @access  Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete contact');
+router.delete("/:id", (req, res) => {
+  res.send("Delete contact");
 });
 
 module.exports = router;
