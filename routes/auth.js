161740f7288b03b2d09d3a3fd000bc5951961911
Economicsnerd <idrissamraoui@protonmail.com>
2020-11-23 08:34:35 -0500

    Authentication logic completed

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,21 @@ const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
 const User = require("../models/User");
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    // select(-password) prevents password from being returned
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route   POST api/auth
