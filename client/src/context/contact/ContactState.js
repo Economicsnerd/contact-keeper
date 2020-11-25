4e7ae07e04bcf04ed759d6607a35a62fc93c958c
Economicsnerd <idrissamraoui@protonmail.com>
2020-11-25 09:30:41 -0500

    Added ContactForm component

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -41,7 +41,10 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   //    Add Contact
-
+  const addContact = (contact) => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
   //    Delete Contact
 
   //    Set current Contact
@@ -53,7 +56,7 @@ const ContactState = (props) => {
   //    Filter Contacts
 
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts }}>
+    <ContactContext.Provider value={{ contacts: state.contacts, addContact }}>
       {props.children}
     </ContactContext.Provider>
   );
