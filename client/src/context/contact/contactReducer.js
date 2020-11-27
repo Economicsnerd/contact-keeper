4b2544e13b822aefd5e1eba03f69758f04cefcf2
Economicsnerd <idrissamraoui@protonmail.com>
2020-11-27 09:01:58 -0500

    Fixed update & delete functionalities

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -19,14 +19,14 @@ export default (state, action) => {
       return {
         ...state,
         contacts: state.contacts.map((contact) =>
-          contact._id === action.payload._id ? action.payload : contact
+          contact.id === action.payload.id ? action.payload : contact
         ),
       };
     case DELETE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.filter(
-          (contact) => contact._id !== action.payload
+          (contact) => contact.id !== action.payload
         ),
       };
     case SET_CURRENT:
