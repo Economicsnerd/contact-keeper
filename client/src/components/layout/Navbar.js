6be928abb08c8309b63a08a36dbd337ebf3a5d67
Economicsnerd <idrissamraoui@protonmail.com>
2020-11-30 09:14:43 -0500

    Added: Login & Register routes + forms

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -15,6 +15,12 @@ export const Navbar = ({ title, icon }) => {
         <li>
           <Link to="/about">About</Link>
         </li>
+        <li>
+          <Link to="/Register">Register</Link>
+        </li>
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
       </ul>
     </div>
   );
