46b8397c5dd5353998e2c7d91dbc51bf9c1311c4
Economicsnerd <idrissamraoui@protonmail.com>
2021-01-11 09:08:43 -0500

    Prepare to deploy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
+
 const app = express();
 
 // Connect DB
@@ -18,6 +20,17 @@ app.get("/", (req, res) =>
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 // Setting up server & PORT
 
